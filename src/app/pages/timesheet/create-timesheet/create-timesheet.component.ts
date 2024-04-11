f089c1efdfac4d46af1e7cefa162faee7971a1fe
prashant-bhamare-gmail <prashantbhamre94@gmail.com>
2024-04-11 14:43:37 +0530

    create timesheet and select project model project activation

--- src/app/pages/timesheet/create-timesheet/create-timesheet.component.ts
+++ src/app/pages/timesheet/create-timesheet/create-timesheet.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-create-timesheet',
   templateUrl: './create-timesheet.component.html',
-  styleUrls: ['./create-timesheet.component.scss']
+  styleUrls: ['./create-timesheet.component.scss'],
+  standalone: true,
+  imports: [MatSlideToggleModule, MatRadioModule, MatCheckboxModule, MatIconModule],
+  
 })
 export class CreateTimesheetComponent implements OnInit {
+ 
 
   constructor() { }
 
--- src/app/pages/timesheet/timesheet.component.ts
+++ src/app/pages/timesheet/timesheet.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateTimesheetModalComponent } from './create-timesheet-modal/create-timesheet-modal.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-timesheet',
   templateUrl: './timesheet.component.html',
-  styleUrls: ['./timesheet.component.scss']
+  styleUrls: ['./timesheet.component.scss'],
+  standalone: true,
+  imports: [MatIconModule],
 })
 export class TimesheetComponent implements OnInit {
 
@@ -20,7 +23,7 @@ export class TimesheetComponent implements OnInit {
 
   createTimeSheet() {
     const activeModal = this.modalService.open(CreateTimesheetModalComponent, {
-      size: 'lg',
+      size: 'sm',
       container: 'nb-layout',
       centered: true,
     });
