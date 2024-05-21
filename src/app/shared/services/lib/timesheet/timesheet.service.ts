0297714902c82f0b44ed81fad08714a7d144dc24
Sagar Mahajan <sagar.mahajan@mindpooltech.net>
2024-05-21 16:19:03 +0530

    bug fix

--- src/app/shared/services/lib/timesheet/timesheet.service.ts
+++ src/app/shared/services/lib/timesheet/timesheet.service.ts
@@ -19,8 +19,8 @@ const httpOptions = {
 export class TimesheetService {
   private userdata;
   private apiBase = '';
-  private timesheetapiBase = 'http://timesheet2.test/';
-  // private timesheetapiBase = 'https://qa-timesheet.rapisurv.com/';
+  // private timesheetapiBase = 'http://timesheet2.test/';
+  private timesheetapiBase = 'https://qa-timesheet.rapisurv.com/';
 
 
   constructor(
